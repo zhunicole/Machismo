f5dc8f5f2621c92b1270a7a77b297e02f2b5d5e1
Nicole Zhu <nicolez@stanford.edu>
2014-04-21 15:21:29 -0700

    added appropriate subcontrollers

---
+++ Matchismo/PlayingGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingGameViewController.h
+//  Matchismo
+//
+//  Created by Nicole Zhu on 4/21/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface PlayingGameViewController : CardGameViewController
+
+@end
---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Nicole Zhu on 4/21/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface SetGameViewController : CardGameViewController
+
+@end
